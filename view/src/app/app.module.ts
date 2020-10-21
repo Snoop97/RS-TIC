c9843fa1210aff24c92506cb81e587d4b0944dd1
Fabian Lopez <flopezs301297@gmail.com>
2020-10-21 10:28:32 -0500

    add new Angular files

---
+++ view/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+// libreria antigua => import { HttpModule } from '@angular/http';
+// nuevo modulo para hacer peticiones AJAX
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { routing, appRoutingProviders} from './app.routing';
+
+//cargando componentes
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    routing,
+    HttpClientModule
+  ],
+  providers: [
+    appRoutingProviders
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
